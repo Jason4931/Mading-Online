0ab034612ea80764ed795beeacfe38941ae3d44c
Jason4931 <emmanuel.jason2008@gmail.com>
2024-05-11 15:00:16 +0700

    riwayat part2

--- riwayat.php
+++ riwayat.php
@@ -5,41 +5,32 @@
     <hr class="d-inline-block float-end w-50 d-none d-sm-block d-lg-none">
     <hr class="d-inline-block float-end w-25 d-block d-sm-none">
     <div class="row">
-        <div class="col-12 col-lg-4 col-sm-6 mt-2">
-            <a href="#">
-                <div class="card bg-dark text-white hover-scale">
-                    <img class="card-img" src="./Image/LogoRPL.jpg" alt="Card image" style="max-height: 200px;">
-                    <img class="card-img-overlay p-0 w-100 h-100" src="./Image/DarkEff.png">
-                    <div class="card-img-overlay d-flex flex-column justify-content-end">
-                        <h5 class="card-title">Peningkatan Layanan Kesehatan Palu Diapresiasi Menkes Budi</h5>
-                        <p class="card-text">Jason4931 - 7 Januari 2024</p>
+        <?php
+        foreach($_SESSION['riwayat'] as $riwayat) {
+            $sql = "SELECT * FROM `informasi` WHERE `id`='$riwayat'";
+            $result = $conn->query($sql);
+            if (mysqli_num_rows($result)>0) {
+                while($row = $result->fetch_assoc()) {
+                    ?>
+                    <div class="col-12 col-lg-4 col-sm-6 mt-2">
+                        <a href="?menu=informasi&id=<?=$row['id']?>">
+                            <div class="card bg-dark text-white hover-scale">
+                                <?php echo '<img src="data:image/jpeg;base64,'.base64_encode($row['gambar']).'" class="card-img" alt="Card image" style="height: 200px;">'; ?>
+                                <img class="card-img-overlay p-0 w-100 h-100" src="./Image/DarkEff.png">
+                                <div class="card-img-overlay d-flex flex-column justify-content-end">
+                                    <h5 class="card-title"><?=$row['judul']?></h5>
+                                    <p class="card-text"><?=$row['penulis']?> - <?=tgl_indo($row['tanggal'])?></p>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-12 col-lg-4 col-sm-6 mt-2">
-            <a href="#">
-                <div class="card bg-dark text-white hover-scale">
-                    <img class="card-img" src="./Image/LogoRPL.jpg" alt="Card image" style="max-height: 200px;">
-                    <img class="card-img-overlay p-0 w-100 h-100" src="./Image/DarkEff.png">
-                    <div class="card-img-overlay d-flex flex-column justify-content-end">
-                        <h5 class="card-title">Peningkatan Layanan Kesehatan Palu Diapresiasi Menkes Budi</h5>
-                        <p class="card-text">Jason4931 - 7 Januari 2024</p>
-                    </div>
-                </div>
-            </a>
-        </div>
-        <div class="col-12 col-lg-4 col-sm-6 mt-2">
-            <a href="#">
-                <div class="card bg-dark text-white hover-scale">
-                    <img class="card-img" src="./Image/LogoRPL.jpg" alt="Card image" style="max-height: 200px;">
-                    <img class="card-img-overlay p-0 w-100 h-100" src="./Image/DarkEff.png">
-                    <div class="card-img-overlay d-flex flex-column justify-content-end">
-                        <h5 class="card-title">Peningkatan Layanan Kesehatan Palu Diapresiasi Menkes Budi</h5>
-                        <p class="card-text">Jason4931 - 7 Januari 2024</p>
-                    </div>
-                </div>
-            </a>
-        </div>
+                    <?php
+                }
+            }
+        }
+        if(empty($_SESSION['riwayat'])) {
+            ?><p class="fs-5">Tidak ada riwayat informasi.</p><?php
+        }
+        ?>
     </div>
 </main>
